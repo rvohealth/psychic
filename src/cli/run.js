362cd22f6238a8791263581b7b33843120cd6770
avocadojesus <>
2021-10-14 19:09:10 -0700

    wip

--- src/cli/run.js
+++ src/cli/run.js
@@ -6,8 +6,11 @@ import l from 'src/singletons/l'
 import 'src/psychic/boot/globals/all'
 
 async function runCLI() {
+  const pkgjson = JSON.parse((await File.read('package.json')))
+  const version = pkgjson.version
   const ascii = fs.readFileSync(`${config.psychicPath}src/psychic/boot/ascii/small.txt`).toString()
   l.logLiteral(chalk.grey(ascii))
+  l.logLiteral(`version ${version}`)
 
   const cli = new CLI()
   await cli.run()
