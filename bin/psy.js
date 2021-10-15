49ac8c4f8cf412f10e9eb391350c92217cc52459
avocadojesus <>
2021-10-15 08:11:13 -0700

    wip

--- bin/psy.js
+++ bin/psy.js
@@ -3,4 +3,4 @@
 var child_process = require('child_process')
 var spawn = child_process.spawn
 
-spawn('yarn run psy', [], { shell: true, stdio: 'inherit' })
+spawn('yarn run psy', process.argv, { shell: true, stdio: 'inherit' })
