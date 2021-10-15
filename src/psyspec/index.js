99006ebb27c1ce8c88e0eba5d53a279d4a2991cb
avocadojesus <>
2021-10-14 17:53:00 -0700

    add comment

--- src/psyspec/index.js
+++ src/psyspec/index.js
@@ -1,3 +1,14 @@
+// NOTE: This file not imported by the core psychic engine. It is instead imported
+// by the user directly using
+//
+// import 'psychic/psyspec'
+//
+// or else by using
+//
+// import { Factory } from 'psychic/psyspec'
+//
+// either way, it will provide a spec framework which is custom-tailored to a real psychic app.
+
 import 'src/psychic/boot/globals/core-spec'
 import 'src/psychic/boot/globals/spec'
 import _Factory from 'src/helpers/factory'
