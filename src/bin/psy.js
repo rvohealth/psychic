b57e407a1a56f8ea97e01df2870f2325c767e423
avocadojesus <>
2021-10-16 09:41:51 -0700

    wip

--- src/bin/psy.js
+++ src/bin/psy.js
@@ -6,6 +6,6 @@ console.log(
   "GDHJSGDJHSGDJSGDHGSHJDGSJHGDHJSGDJHSGDHJSGDJGHS",
   process.argv,
   process.argv.slice(2),
-  process.env.INIT_CWD,
+  'init: ' + process.env.INIT_CWD || "nil",
 )
 // spawn(`cd ${path.resolve(__dirname, '../../')} && ORIGINAL_CWD=${process.env.INIT_CWD} yarn run psy ${process.argv.slice(2).join(' ')}`, [], { shell: true, stdio: 'inherit' })
