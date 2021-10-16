8b4381bdf69185bad34e839c1a044c8871c9c648
avocadojesus <>
2021-10-16 09:16:00 -0700

    wip

--- src/bin/psy.js
+++ src/bin/psy.js
@@ -2,5 +2,9 @@
 
 import path from 'path'
 import spawn from 'src/helpers/spawn'
-console.log("GDHJSGDJHSGDJSGDHGSHJDGSJHGDHJSGDJHSGDHJSGDJGHS", path.resolve(__dirname, '../../'), process.argv.join(' '))
+console.log(
+  "GDHJSGDJHSGDJSGDHGSHJDGSJHGDHJSGDJHSGDHJSGDJGHS",
+  process.argv,
+  process.argv.slice(2)
+)
 // spawn(`cd ${path.resolve(__dirname, '../../')} && yarn run psy ${process.argv.join(' ')}`, [], { shell: true, stdio: 'inherit' })
