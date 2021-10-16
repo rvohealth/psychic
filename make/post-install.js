2f314063a3011ed6995d9ca72a2c220932ccd897
avocadojesus <>
2021-10-16 08:26:02 -0700

    wip

--- make/post-install.js
+++ make/post-install.js
@@ -2,7 +2,7 @@ import File from './helpers/file'
 
 async function postNPMInstall() {
   await File.touch('.env.development')
-  console.log('DONNNNEEEE', __dirname)
+  console.log('DONNNNEEEE', __dirname, __filename)
 }
 
 postNPMInstall()
