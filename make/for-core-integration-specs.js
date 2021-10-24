7c4ebe295c82a6e3651bfdcfdfcf7037249d4937
avocadojesus <>
2021-10-23 17:53:10 -0700

    wip

--- make/for-core-integration-specs.js
+++ make/for-core-integration-specs.js
@@ -1,6 +1,6 @@
 import Packager from './packager'
 const packager = new Packager({
-  appDir: 'tmp/integrationtestapp/app/',
+  appDir: 'template/psychic-app/app/',
   prefix: '.dist/testapp/',
   // originalPathPrefix: 'spec/support/testapp/',
 })
