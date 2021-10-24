beb497c0529b16c0a7fef0a64085375c051bfa1a
avocadojesus <>
2021-10-23 17:49:36 -0700

    wip

--- make/for-core-integration-specs.js
+++ make/for-core-integration-specs.js
@@ -1,6 +1,6 @@
 import Packager from './packager'
 const packager = new Packager({
-  appDir: 'tmp/integrationtestapp/',
+  appDir: 'tmp/integrationtestapp/app/',
   prefix: '.dist/testapp/',
   // originalPathPrefix: 'spec/support/testapp/',
 })
