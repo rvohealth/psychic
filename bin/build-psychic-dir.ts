56155ebb7d1d3e4d887772473855cc9679423dfd
avocadojesus <jgarbey@gmail.com>
2023-04-15 09:35:21 -0700

    build-howl-dir build-psychic-dir

--- bin/build-psychic-dir.ts
+++ bin/build-psychic-dir.ts
@@ -2,14 +2,14 @@
 
 import * as fs from 'fs'
 
-async function guaranteeCache() {
+async function buildPsychicDir() {
   if (fs.existsSync(rootPath() + '/.howl'))
     fs.rmSync(rootPath() + '/.howl/', { force: true, recursive: true })
 
   fs.mkdirSync(rootPath() + '/.howl')
 
   // otherwise, we use the buildGlobals provided by our core spec layer
-  const boilerplateSubpath = process.env.CORE_DEVELOPMENT === '1' ? 'howl-core-dev' : 'howl'
+  const boilerplateSubpath = process.env.CORE_DEVELOPMENT === '1' ? 'howl-core-dev' : 'howll'
 
   fs.writeFileSync(
     rootPath() + '/.howl/buildGlobals.ts',
@@ -27,7 +27,7 @@ async function guaranteeCache() {
     if (!fs.existsSync(filePath)) fs.writeFileSync(filePath, 'export default {}')
   })
 }
-guaranteeCache()
+buildPsychicDir()
 
 function rootPath() {
   if (process.env.CORE_DEVELOPMENT === '1') {
