d55da74ad4e0afd6e38e1ae2c028218f0d23f55c
avocadojesus <jgarbey@gmail.com>
2023-04-15 12:10:46 -0700

    import encryption module specs from howl

---
+++ spec/unit/encryption/hash.spec.ts
@@ -0,0 +1,22 @@
+import * as bcrypt from 'bcrypt'
+import Hash from '../../../src/encryption/hash'
+
+// TODO: finish spec, ensuring that encryption key is passed
+const originalEncryptionKey = process.env.APP_ENCRYPTION_KEY
+
+describe('Hash', () => {
+  describe('#gen', () => {
+    beforeEach(() => {
+      jest.spyOn(bcrypt, 'hash')
+    })
+
+    afterEach(() => {
+      process.env.APP_ENCRYPTION_KEY = originalEncryptionKey
+    })
+
+    it('signs the provided string using jwt', () => {
+      Hash.gen('how')
+      expect(bcrypt.hash).toHaveBeenCalledWith('how', 11)
+    })
+  })
+})
