7f5db12868cc0f46f339454653a6627a8a6f439c
avocadojesus <jgarbey@gmail.com>
2023-04-15 21:49:39 -0700

    import cable specs from howl

---
+++ spec/unit/cable/start.spec.ts
@@ -0,0 +1,43 @@
+import * as http from 'http'
+import Cable from '../../../src/cable'
+import HowlServer from '../../../src/server'
+import { Server } from 'http'
+
+describe('cable#start', () => {
+  let server: HowlServer
+  let cable: Cable
+  let httpServer: any
+  beforeEach(async () => {
+    server = new HowlServer()
+    cable = new Cable(server.app)
+    httpServer = http.createServer(server.app)
+    await cable.connect()
+
+    // @ts-ignore
+    jest.spyOn(cable.io, 'on').mockImplementation(() => '' as any)
+    jest.spyOn(cable, 'listen').mockImplementation(async () => {})
+    jest.spyOn(cable, 'connect')
+  })
+
+  it('calls cable#connect to establish io and http server if they arent already established', async () => {
+    await cable.start()
+    expect(cable.connect).toHaveBeenCalled()
+  })
+
+  it('calls cable#listen to start http server and sidechain io server to it', async () => {
+    await cable.start()
+    expect(cable.listen).toHaveBeenCalledWith({ port: 7777, withReact: false, reactPort: 3000 })
+  })
+
+  it('attaches a socket io instance to the cable instance', async () => {
+    await cable.start()
+    // @ts-ignore
+    expect(cable.io.on).toHaveBeenCalled()
+  })
+
+  context('howl is configured for redis as well', () => {
+    it('creates additional redis bindings', async () => {
+      // TODO: add coverage here.
+    })
+  })
+})
