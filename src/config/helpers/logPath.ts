2fd3f4bd7b9b4765d806e5f7327c2db9c6887dd2
avocadojesus <jgarbey@gmail.com>
2023-05-06 22:02:51 -0700

    remove preceding slash

--- src/config/helpers/logPath.ts
+++ src/config/helpers/logPath.ts
@@ -1,5 +1,5 @@
 import absoluteFilePath from '../../helpers/absoluteFilePath'
 
 export default function logPath() {
-  return absoluteFilePath(`/logs/log.${process.env.NODE_ENV}`)
+  return absoluteFilePath(`logs/log.${process.env.NODE_ENV}`)
 }
