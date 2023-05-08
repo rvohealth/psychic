c08f7db34179fa731150b8457b20c3d0ee319a93
avocadojesus <jgarbey@gmail.com>
2023-05-08 14:47:28 -0700

    remove problematic line

--- bin/cli.ts
+++ bin/cli.ts
@@ -157,10 +157,9 @@ program
       // intentionally ignore, since we expect this dir to be empty.
     }
 
-    await sspawn(`yarn dream sync:existing${coreSuffix(program.args)}`)
-
     // TODO: figure out why this throws DB error. For now, this is manually run by the
     // consuming app.
+    // await sspawn(`yarn dream sync:existing${coreSuffix(program.args)}`)
     // await sspawn(`yarn dream sync:all`)
   })
 
