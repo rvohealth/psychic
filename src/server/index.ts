3c07ae9fc46dfd45d9c838f74b43dba0a26a25ab
avocadojesus <jgarbey@gmail.com>
2023-06-27 09:00:11 -0700

    only log if not in test env

--- src/server/index.ts
+++ src/server/index.ts
@@ -81,10 +81,12 @@ export default class PsychicServer {
     } else {
       await new Promise(async accept => {
         this.server = this.app.listen(port, async () => {
-          log.welcome(port)
-          await log.write(`psychic dev server started at port ${port}`)
+          if (process.env.NODE_ENV === 'test') {
+            log.welcome(port)
+            await log.write(`psychic dev server started at port ${port}`)
+            if (withReact) await log.write(`react dev server on port ${reactPort}`)
+          }
 
-          if (withReact) await log.write(`react dev server on port ${reactPort}`)
           accept({})
         })
       })
