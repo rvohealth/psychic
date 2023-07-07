a4ed1ef7d93863899e6ef0e63c75acf28e314b29
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2023-07-07 12:18:26 -0400

    Revert "Eliminate src/src"
    This reverts commit f72fc67548e4e1f86fbeca79330a9e6c3ff9b6c5.

--- src/helpers/absoluteFilePath.ts
+++ src/helpers/absoluteFilePath.ts
@@ -4,14 +4,10 @@
 // build to dist, since directory structures morph in those contexts.
 import path from 'path'
 
-export default function absoluteSrcPath(filePath: string) {
-  const absPath =
-    process.env.PSYCHIC_CORE_DEVELOPMENT === '1'
-      ? path.join(process.cwd(), 'test-app', filePath)
-      : process.env.EXECUTED_ON_BEHALF_OF_CONSUMING_APP_BY_CLI === '1'
-      ? path.join(process.cwd(), '..', '..', 'src', filePath)
-      : path.join(process.cwd(), 'src', filePath)
-
-  const slash = path.sep
-  return absPath.replace(`${slash}api${slash}src${slash}src${slash}`, `${slash}api${slash}src${slash}`)
+export default function absoluteFilePath(filePath: string) {
+  return process.env.PSYCHIC_CORE_DEVELOPMENT === '1'
+    ? path.join(process.cwd(), filePath)
+    : process.env.EXECUTED_ON_BEHALF_OF_CONSUMING_APP_BY_CLI === '1'
+    ? path.join(process.cwd(), '..', '..', filePath)
+    : path.join(process.cwd(), filePath)
 }
