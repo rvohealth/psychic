fe533b8d298980125968abd8acbd53f36df6d099
avocadojesus <jgarbey@gmail.com>
2023-08-08 18:00:29 -0700

    allow log path override

--- src/config/helpers/logPath.ts
+++ src/config/helpers/logPath.ts
@@ -1,5 +1,5 @@
 import absoluteFilePath from '../../helpers/absoluteFilePath'
 
 export default function logPath() {
-  return absoluteFilePath(`logs/log.${process.env.NODE_ENV}`)
+  return process.env.PSYCHIC_LOG_PATH || absoluteFilePath(`logs/log.${process.env.NODE_ENV}`)
 }
