9ab7812255800f5dbe9697d627e35aecf529706e
avocadojesus <jgarbey@gmail.com>
2023-08-10 00:31:14 -0700

    add debug

--- bin/cli/helpers/yarncmdRunByAppConsumer.ts
+++ bin/cli/helpers/yarncmdRunByAppConsumer.ts
@@ -4,5 +4,9 @@ import yarnCwd from './yarmCwd'
 export default function yarncmdRunByAppConsumer(command: string, programArgs: string[]) {
   const coreSuffixFlag = coreSuffix(programArgs)
   const nodeEnvPrefix = process.env.NODE_ENV ? `NODE_ENV=${process.env.NODE_ENV} ` : ''
-  return `${nodeEnvPrefix}yarn ${yarnCwd(programArgs)}${command}${coreSuffixFlag}`
+  const cmd = `${nodeEnvPrefix}yarn ${yarnCwd(programArgs)}${command}${coreSuffixFlag}`
+  if (process.env.DEBUG === '1') {
+    console.log('About to run command: ', cmd)
+  }
+  return cmd
 }
