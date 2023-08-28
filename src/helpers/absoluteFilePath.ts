14d1f00bb910df0118176771dc8ec69f7ff92996
avocadojesus <jgarbey@gmail.com>
2023-08-27 20:44:17 -0700

    remove dead code

--- src/helpers/absoluteFilePath.ts
+++ src/helpers/absoluteFilePath.ts
@@ -10,9 +10,4 @@ export default function absoluteFilePath(filePath: string) {
       [Psychic]: Must set APP_ROOT_PATH before using psychic
     `
   return path.join(process.env.APP_ROOT_PATH!, filePath)
-  // return process.env.PSYCHIC_CORE_DEVELOPMENT === '1'
-  //   ? path.join(process.cwd(), filePath)
-  //   : process.env.EXECUTED_ON_BEHALF_OF_CONSUMING_APP_BY_CLI === '1'
-  //   ? path.join(process.cwd(), '..', '..', filePath)
-  //   : path.join(process.cwd(), filePath)
 }
