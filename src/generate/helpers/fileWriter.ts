8068b7e29570eda48b80dd4cc61ebe56e16ec17f
avocadojesus <jgarbey@gmail.com>
2023-08-31 10:18:38 -0700

    sync up fileWriter with dream implementation

--- src/generate/helpers/fileWriter.ts
+++ src/generate/helpers/fileWriter.ts
@@ -1,22 +1,18 @@
 import pluralize from 'pluralize'
 import fs from 'fs/promises'
-import { pascalize, compact } from 'dream'
-import path from 'path'
+import { pascalize } from 'dream'
 
 export default async function fileWriter(
   filePath: string,
-  filePostfix: 'Controller' | 'Serializer',
+  filePostfix: 'Serializer',
   fileExtension: '.ts' | '.spec.ts',
   pluralizeBeforePostfix: boolean,
-  directoryPrefix: 'app/controllers' | 'app/serializers' | 'spec/unit/controllers' | 'spec/unit/serializers',
-  rootPath: string | null = null,
   contentFunction: (...args: any[]) => Promise<string> | string,
-  contentFunctionAttrs: any[] = []
+  rootPath: string,
+  contentFunctionAttrs?: any[]
 ) {
   const thisfs = fs ? fs : await import('fs/promises')
-  const srcPath =
-    fileExtension === '.spec.ts' ? '' : process.env.PSYCHIC_CORE_DEVELOPMENT === '1' ? null : 'src'
-  const newfileBasePath = path.join(...compact([rootPath, srcPath, directoryPrefix]))
+  const srcPath = process.env.DREAM_CORE_DEVELOPMENT === '1' ? 'test-app' : 'src'
   const fullyQualifiedNewfileClassName = pluralizeBeforePostfix
     ? `${pluralize(filePath)}${filePostfix}`
     : `${filePath}${filePostfix}`
@@ -26,25 +22,17 @@ export default async function fileWriter(
     .map(str => pascalize(str))
     .join('/')
   const dirPartsRelativeToTypeRoot = filepathRelativeToTypeRoot.split('/').slice(0, -1)
-  const newfileFileContents = await contentFunction(newfileClassName, ...contentFunctionAttrs)
+  const newfileFileContents = await contentFunction(newfileClassName, ...(contentFunctionAttrs || []))
 
   // if they are generating a nested newfile path,
   // we need to make sure the nested directories exist
   if (!!dirPartsRelativeToTypeRoot.length) {
-    const fullDirectoryPath = [...newfileBasePath.split('/'), ...dirPartsRelativeToTypeRoot].join('/')
+    const fullDirectoryPath = [...rootPath.split('/'), ...dirPartsRelativeToTypeRoot].join('/')
     await thisfs.mkdir(fullDirectoryPath, { recursive: true })
   }
 
-  const fullNewfilePath = `${newfileBasePath}/${filepathRelativeToTypeRoot}${fileExtension}`
-  const rootRelativeNewfilePath = fullNewfilePath.replace(
-    new RegExp(`^.*${directoryPrefix}`),
-    directoryPrefix
-  )
-
-  console.log(
-    `generating ${filePostfix.toLowerCase()}${
-      fileExtension === '.spec.ts' ? ' spec' : ''
-    }: ${rootRelativeNewfilePath}`
-  )
+  const fullNewfilePath = `${rootPath}/${filepathRelativeToTypeRoot}${fileExtension}`
+  const rootRelativeNewfilePath = fullNewfilePath.replace(new RegExp(`^.*${rootPath}`), rootPath)
+  console.log(`generating ${filePostfix.toLowerCase()}: ${rootRelativeNewfilePath}`)
   await thisfs.writeFile(fullNewfilePath, newfileFileContents)
 }
