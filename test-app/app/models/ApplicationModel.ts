a01581d01afc1c4d5466ec10a65de38c6dadf172
avocadojesus <jgarbey@gmail.com>
2023-09-13 18:53:54 -0700

    bump dream, stop syncing db credentials

--- test-app/app/models/ApplicationModel.ts
+++ test-app/app/models/ApplicationModel.ts
@@ -1,39 +1,18 @@
-import { Dream } from 'dream'
-import { DBClass, DBColumns, DBTypeCache, InterpretedDBClass } from '../../db/schema'
-import SyncedAssociationsVal, {
-  SyncedAssociations,
-  SyncedBelongsToAssociations,
-  VirtualColumns,
-} from '../../db/associations'
+import { Dream, Dreamconf } from 'dream'
+import { DBClass } from '../../db/schema'
+import SyncedAssociationsVal, { SyncedAssociations } from '../../db/associations'
+import dreamconf from '../../conf/dreamconf'
 
 export default class ApplicationModel extends Dream {
   public get DB() {
     return new DBClass()
   }
 
-  public get interpretedDB(): InterpretedDBClass {
-    return new InterpretedDBClass()
-  }
-
   public get syncedAssociations(): SyncedAssociations {
     return SyncedAssociationsVal as SyncedAssociations
   }
 
-  public get syncedBelongsToAssociations(): SyncedBelongsToAssociations {
-    // this just returns an interface anyways, so no need for a real value
-    return {} as SyncedBelongsToAssociations
-  }
-
-  public get virtualColumns(): VirtualColumns {
-    // this just returns an interface anyways, so no need for a real value
-    return {} as VirtualColumns
-  }
-
-  public get dbColumns(): typeof DBColumns {
-    return DBColumns
-  }
-
-  public get dbTypeCache(): typeof DBTypeCache {
-    return DBTypeCache
+  public get dreamconf(): Dreamconf {
+    return dreamconf
   }
 }
---
+++ test-app/conf/dreamconf.ts
@@ -0,0 +1,77 @@
+import { Dreamconf } from 'dream'
+import SyncedAssociationsVal, {
+  SyncedAssociations,
+  SyncedBelongsToAssociations,
+  VirtualColumns,
+} from '../db/associations'
+import { DBClass, DBColumns, DBTypeCache, InterpretedDBClass } from '../db/schema'
+
+const dreamconf = new Dreamconf({
+  DB: new DBClass(),
+  interpretedDB: new InterpretedDBClass(),
+  syncedAssociations: SyncedAssociationsVal as SyncedAssociations,
+  syncedBelongsToAssociations: {} as SyncedBelongsToAssociations,
+  virtualColumns: {} as VirtualColumns,
+  dbColumns: DBColumns,
+  dbTypeCache: DBTypeCache,
+  env: {
+    db: {
+      development: {
+        primary: {
+          user: 'DB_USER',
+          password: 'DB_PASSWORD',
+          host: 'DB_HOST',
+          name: 'DB_NAME',
+          port: 'DB_PORT',
+          use_ssl: 'DB_USE_SSL',
+        },
+        replica: {
+          user: 'DB_USER',
+          password: 'DB_PASSWORD',
+          host: 'REPLICA_DB_HOST',
+          name: 'REPLICA_DB_NAME',
+          port: 'DB_PORT',
+          use_ssl: 'DB_USE_SSL',
+        },
+      },
+      test: {
+        primary: {
+          user: 'DB_USER',
+          password: 'DB_PASSWORD',
+          host: 'DB_HOST',
+          name: 'DB_NAME',
+          port: 'DB_PORT',
+          use_ssl: 'DB_USE_SSL',
+        },
+        replica: {
+          user: 'DB_USER',
+          password: 'DB_PASSWORD',
+          host: 'REPLICA_DB_HOST',
+          name: 'REPLICA_DB_NAME',
+          port: 'DB_PORT',
+          use_ssl: 'DB_USE_SSL',
+        },
+      },
+      production: {
+        primary: {
+          user: 'DB_USER',
+          password: 'DB_PASSWORD',
+          host: 'DB_HOST',
+          name: 'DB_NAME',
+          port: 'DB_PORT',
+          use_ssl: 'DB_USE_SSL',
+        },
+        replica: {
+          user: 'DB_USER',
+          password: 'DB_PASSWORD',
+          host: 'REPLICA_DB_HOST',
+          name: 'REPLICA_DB_NAME',
+          port: 'DB_PORT',
+          use_ssl: 'DB_USE_SSL',
+        },
+      },
+    },
+  },
+})
+
+export default dreamconf
