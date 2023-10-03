792b563e1c5301d72967fae5be6555fea8e2d18b
avocadojesus <jgarbey@gmail.com>
2023-10-03 12:26:25 -0700

    fix base path for dream proxy

--- boot/cli/helpers/dreamjsOrDreamtsCmd.ts
+++ boot/cli/helpers/dreamjsOrDreamtsCmd.ts
@@ -10,7 +10,7 @@ export default function dreamjsOrDreamtsCmd(
   const useTsnode = programArgs.includes('--tsnode') || process.env.TS_SAFE === '1'
   const dreamCmd = useTsnode ? 'dreamts' : 'dreamjs'
   const omitDistFromPathEnv = useTsnode ? 'PSYCHIC_OMIT_DIST_FOLDER=1 ' : ''
-  const basepath = process.env.PSYCHIC_CORE_DEVELOPMENT === '1' ? '' : '../../'
+  const basepath = process.env.PSYCHIC_CORE_DEVELOPMENT === '1' ? '' : '../../../'
   if (useTsnode) cmdArgs.push('--tsnode')
 
   const fullcmd = `${coreDevFlag}${omitDistFromPathEnv}yarn --cwd=${basepath}node_modules/@rvohealth/dream ${dreamCmd} ${cmd} ${cmdArgs.join(
