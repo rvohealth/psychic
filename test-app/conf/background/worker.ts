e5d4e196704aa658403fd0e0f3aa12109cc4389b
avocadojesus <jgarbey@gmail.com>
2023-10-31 08:53:19 -0700

    bump dream, add sigint handler for workers
    related https://rvohealth.atlassian.net/browse/PDTC-2217

--- test-app/conf/background/worker.ts
+++ test-app/conf/background/worker.ts
@@ -1,5 +1,13 @@
 import { WorkerOptions } from 'bullmq'
 
 export default async function () {
-  return {} as WorkerOptions
+  return {
+    removeOnComplete: {
+      age: 3600, // keep up to 1 hour
+      count: 1000, // keep up to 1000 jobs
+    },
+    removeOnFail: {
+      age: 24 * 3600 * 31, // keep up to 31 days
+    },
+  } as WorkerOptions
 }
