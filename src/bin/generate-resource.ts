1a3ca98a66f7e1c7b64f87ffbb3a1baee3c88deb
avocadojesus <jgarbey@gmail.com>
2024-03-21 17:41:02 -0700

    add debug

--- src/bin/generate-resource.ts
+++ src/bin/generate-resource.ts
@@ -1,6 +1,7 @@
 import generateResource from '../generate/resource'
 
 async function _generateResource() {
+  console.log(process.argv)
   let route = process.argv[2]
   let name = process.argv[3]
   const args = process.argv.slice(4, process.argv.length)
