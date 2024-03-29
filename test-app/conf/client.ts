78daebf33c58f4d43c9f4adaa06ddcb2856e956b
avocadojesus <jgarbey@gmail.com>
2024-03-28 21:46:07 -0700

    remove dead conf files

--- test-app/conf/client.ts
+++
@@ -1,7 +0,0 @@
-export default async () => {
-  return {
-    host:
-      process.env.CLIENT_HOST ||
-      (process.env.NODE_ENV === 'test' ? 'http://localhost:7778' : 'http://localhost:3000'),
-  }
-}
--- test-app/conf/server.ts
+++
@@ -1,5 +0,0 @@
-export default async () => {
-  return {
-    host: process.env.API_HOST || 'http://localhost:7777',
-  }
-}
