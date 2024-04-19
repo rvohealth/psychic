65a6570550a5f8795ec7649678049d23f46de8fc
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2024-04-19 16:17:32 -0500

    Omit --tsnode args

--- src/bin/generate-resource.ts
+++ src/bin/generate-resource.ts
@@ -3,9 +3,8 @@ import generateResource from '../generate/resource'
 async function _generateResource() {
   const route = process.argv[2]
   const name = process.argv[3]
-  // omit the last arg, since it is for some reason a clustering of all args put together,
-  // and will inevitably confuse the helper function into generating nonsensical model attributes
-  const args = process.argv.slice(4, process.argv.length - 1)
+  const indexOfTsNode = process.argv.findIndex(str => str === '--tsnode')
+  const args = indexOfTsNode ? process.argv.slice(4, indexOfTsNode) : process.argv.slice(4)
   await generateResource(route, name, args)
 }
 
