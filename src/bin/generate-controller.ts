e9b71c6e14b95b885219b104a1f0b39db8f3a872
avocadojesus <jgarbey@gmail.com>
2024-05-07 10:05:18 -0700

    properly truncate args before generating controller

--- src/bin/generate-controller.ts
+++ src/bin/generate-controller.ts
@@ -3,11 +3,12 @@ import generateController from '../generate/controller'
 async function _generateController() {
   const route = process.argv[2]
   const name = process.argv[3]
-  const methods = process.argv.slice(4, process.argv.length)
+  const indexOfTsNode = process.argv.findIndex(str => str === '--tsnode')
+  const methods = indexOfTsNode ? process.argv.slice(4, indexOfTsNode) : process.argv.slice(4)
   await generateController(
     route,
     name,
-    methods.filter(method => !['--core'].includes(method)),
+    methods.filter(method => !['--core'].includes(method))
   )
 }
 
