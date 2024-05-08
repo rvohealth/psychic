f786a4e838a14b08b66598ae7b9f6c84e52ddedd
avocadojesus <jgarbey@gmail.com>
2024-05-08 14:14:42 -0700

    fix linting errors

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -110,7 +110,7 @@ export default class PsychicController {
       config,
     }: {
       config: PsychicConfig
-    }
+    },
   ) {
     this.req = req
     this.res = res
@@ -162,7 +162,7 @@ export default class PsychicController {
       JSON.stringify({
         id: user.primaryKeyValue,
         modelKey: await getModelKey(user.constructor as typeof Dream),
-      })
+      }),
     )
   }
 
@@ -187,17 +187,16 @@ export default class PsychicController {
   }
 
   public json<T>(
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     data: T,
-    opts: RenderOptions<T> = {}
+    opts: RenderOptions<T> = {},
   ): // eslint-disable-next-line @typescript-eslint/no-explicit-any
   any {
     if (Array.isArray(data))
       return this.res.json(
         data.map(d =>
           // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-          this.singleObjectJson(d, opts)
-        )
+          this.singleObjectJson(d, opts),
+        ),
       )
     return this.res.json(this.singleObjectJson(data, opts))
   }
@@ -215,14 +214,12 @@ export default class PsychicController {
     return this.json(data, opts)
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public created<T>(data: T = {} as T, opts: RenderOptions<T> = {}) {
     this.res.status(201)
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return this.json(data, opts)
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public accepted<T>(data: T = {} as T, opts: RenderOptions<T> = {}) {
     this.res.status(202)
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
@@ -260,7 +257,7 @@ export default class PsychicController {
   public badRequest(data: SerializerResult = {}) {
     throw new BadRequest(
       'The server cannot or will not process the request due to something that is perceived to be a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing).',
-      data
+      data,
     )
   }
 
@@ -293,7 +290,7 @@ export default class PsychicController {
   public internalServerError(data: SerializerResult = {}) {
     throw new InternalServerError(
       'The server has encountered a situation it does not know how to handle.',
-      data
+      data,
     )
   }
 
@@ -301,7 +298,7 @@ export default class PsychicController {
   public notImplemented(data: SerializerResult = {}) {
     throw new InternalServerError(
       'The request method is not supported by the server and cannot be handled. The only methods that servers are required to support (and therefore that must not return this code) are GET and HEAD',
-      data
+      data,
     )
   }
 
@@ -318,7 +315,7 @@ export default class PsychicController {
 
   public async runBeforeActionsFor(action: string) {
     const beforeActions = (this.constructor as typeof PsychicController).controllerHooks.filter(hook =>
-      hook.shouldFireForAction(action)
+      hook.shouldFireForAction(action),
     )
 
     for (const hook of beforeActions) {
@@ -339,23 +336,23 @@ export class ControllerSerializerIndex {
   public add(
     ControllerClass: typeof PsychicController,
     SerializerClass: typeof DreamSerializer,
-    ModelClass: typeof Dream
+    ModelClass: typeof Dream,
   ) {
     this.associations.push([ControllerClass, SerializerClass, ModelClass])
   }
 
   public lookupModel(ControllerClass: typeof PsychicController, ModelClass: typeof Dream) {
     return this.associations.find(
-      association => association[0] === ControllerClass && association[2] === ModelClass
+      association => association[0] === ControllerClass && association[2] === ModelClass,
     )
   }
 
   public lookupSerializer(
     ControllerClass: typeof PsychicController,
-    SerializerClass: typeof DreamSerializer
+    SerializerClass: typeof DreamSerializer,
   ) {
     return this.associations.find(
-      association => association[0] === ControllerClass && association[1] === SerializerClass
+      association => association[0] === ControllerClass && association[1] === SerializerClass,
     )
   }
 }
