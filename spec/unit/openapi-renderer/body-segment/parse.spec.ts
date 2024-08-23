3eea7a4c161c269512d4f61fc80388b0e87e77ed
avocadojesus <jgarbey@gmail.com>
2024-08-22 23:26:25 -0700

    add support for flattened renders one associations

--- spec/unit/openapi-renderer/body-segment/parse.spec.ts
+++ spec/unit/openapi-renderer/body-segment/parse.spec.ts
@@ -1,11 +1,10 @@
 import OpenapiBodySegmentRenderer from '../../../../src/openapi-renderer/body-segment'
+import { OpenapiBodySegment } from '../../../../src/openapi-renderer/serializer'
 
 describe('OpenapiBodySegmentRenderer', () => {
   describe('#parse', () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const subject = (bodySegment: any) =>
+    const subject = (bodySegment: OpenapiBodySegment) =>
       new OpenapiBodySegmentRenderer({
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         bodySegment,
         serializers: {},
         schemaDelimeter: '',
@@ -27,74 +26,98 @@ describe('OpenapiBodySegmentRenderer', () => {
       expect(subject('decimal')).toEqual({ type: 'number', format: 'decimal' })
     })
 
-    it('can handle array types', () => {
-      expect(subject('string[]')).toEqual({ type: 'array', items: { type: 'string' } })
-      expect(subject('boolean[]')).toEqual({ type: 'array', items: { type: 'boolean' } })
-      expect(subject('number[]')).toEqual({ type: 'array', items: { type: 'number' } })
-      expect(subject('integer[]')).toEqual({ type: 'array', items: { type: 'integer' } })
-      expect(subject('double[]')).toEqual({ type: 'array', items: { type: 'number', format: 'double' } })
-      expect(subject('decimal[]')).toEqual({ type: 'array', items: { type: 'number', format: 'decimal' } })
-      expect(subject('date[]')).toEqual({ type: 'array', items: { type: 'string', format: 'date' } })
-      expect(subject('date-time[]')).toEqual({
-        type: 'array',
-        items: { type: 'string', format: 'date-time' },
+    context('objects', () => {
+      it('preserves description', () => {
+        expect(subject({ type: 'string', description: 'hi' })).toEqual({ type: 'string', description: 'hi' })
+      })
+
+      it('can handle object primitives', () => {
+        expect(subject({ type: 'string' })).toEqual({ type: 'string' })
+        expect(subject({ type: 'boolean' })).toEqual({ type: 'boolean' })
+        expect(subject({ type: 'number' })).toEqual({ type: 'number' })
+        expect(subject({ type: 'integer' })).toEqual({ type: 'integer' })
+        expect(subject({ type: 'double' })).toEqual({ type: 'number', format: 'double' })
+        expect(subject({ type: 'decimal' })).toEqual({ type: 'number', format: 'decimal' })
+        expect(subject({ type: 'date' })).toEqual({ type: 'string', format: 'date' })
+        expect(subject({ type: 'date-time' })).toEqual({ type: 'string', format: 'date-time' })
+      })
+
+      it('can handle objects with primitive arrays for types', () => {
+        expect(subject({ type: 'string[]' })).toEqual({ type: 'array', items: { type: 'string' } })
+        expect(subject({ type: 'boolean[]' })).toEqual({ type: 'array', items: { type: 'boolean' } })
+        expect(subject({ type: 'number[]' })).toEqual({ type: 'array', items: { type: 'number' } })
+        expect(subject({ type: 'integer[]' })).toEqual({ type: 'array', items: { type: 'integer' } })
+        expect(subject({ type: 'double[]' })).toEqual({
+          type: 'array',
+          items: { type: 'number', format: 'double' },
+        })
+        expect(subject({ type: 'decimal[]' })).toEqual({
+          type: 'array',
+          items: { type: 'number', format: 'decimal' },
+        })
+        expect(subject({ type: 'date[]' })).toEqual({
+          type: 'array',
+          items: { type: 'string', format: 'date' },
+        })
+        expect(subject({ type: 'date-time[]' })).toEqual({
+          type: 'array',
+          items: { type: 'string', format: 'date-time' },
+        })
       })
     })
 
-    it('can handle object primitives', () => {
-      expect(subject({ type: 'string' })).toEqual({ type: 'string' })
-      expect(subject({ type: 'boolean' })).toEqual({ type: 'boolean' })
-      expect(subject({ type: 'number' })).toEqual({ type: 'number' })
-      expect(subject({ type: 'integer' })).toEqual({ type: 'integer' })
-      expect(subject({ type: 'double' })).toEqual({ type: 'number', format: 'double' })
-      expect(subject({ type: 'decimal' })).toEqual({ type: 'number', format: 'decimal' })
-      expect(subject({ type: 'date' })).toEqual({ type: 'string', format: 'date' })
-      expect(subject({ type: 'date-time' })).toEqual({ type: 'string', format: 'date-time' })
-    })
+    context('arrays', () => {
+      it('preserves description', () => {
+        expect(subject({ type: 'string[]', description: 'hi' })).toEqual({
+          type: 'array',
+          description: 'hi',
+          items: { type: 'string' },
+        })
 
-    it('can handle object primitive arrays', () => {
-      expect(subject({ type: 'string[]' })).toEqual({ type: 'array', items: { type: 'string' } })
-      expect(subject({ type: 'boolean[]' })).toEqual({ type: 'array', items: { type: 'boolean' } })
-      expect(subject({ type: 'number[]' })).toEqual({ type: 'array', items: { type: 'number' } })
-      expect(subject({ type: 'integer[]' })).toEqual({ type: 'array', items: { type: 'integer' } })
-      expect(subject({ type: 'double[]' })).toEqual({
-        type: 'array',
-        items: { type: 'number', format: 'double' },
+        expect(
+          subject({ type: 'array', description: 'hi', items: { type: 'string', description: 'hi2' } }),
+        ).toEqual({
+          type: 'array',
+          description: 'hi',
+          items: { type: 'string', description: 'hi2' },
+        })
       })
-      expect(subject({ type: 'decimal[]' })).toEqual({
-        type: 'array',
-        items: { type: 'number', format: 'decimal' },
-      })
-      expect(subject({ type: 'date[]' })).toEqual({
-        type: 'array',
-        items: { type: 'string', format: 'date' },
-      })
-      expect(subject({ type: 'date-time[]' })).toEqual({
-        type: 'array',
-        items: { type: 'string', format: 'date-time' },
-      })
-    })
 
-    it('can handle object primitive arrays', () => {
-      expect(subject({ type: 'string[]' })).toEqual({ type: 'array', items: { type: 'string' } })
-      expect(subject({ type: 'boolean[]' })).toEqual({ type: 'array', items: { type: 'boolean' } })
-      expect(subject({ type: 'number[]' })).toEqual({ type: 'array', items: { type: 'number' } })
-      expect(subject({ type: 'integer[]' })).toEqual({ type: 'array', items: { type: 'integer' } })
-      expect(subject({ type: 'double[]' })).toEqual({
-        type: 'array',
-        items: { type: 'number', format: 'double' },
+      it('can handle primitive array types', () => {
+        expect(subject('string[]')).toEqual({ type: 'array', items: { type: 'string' } })
+        expect(subject('boolean[]')).toEqual({ type: 'array', items: { type: 'boolean' } })
+        expect(subject('number[]')).toEqual({ type: 'array', items: { type: 'number' } })
+        expect(subject('integer[]')).toEqual({ type: 'array', items: { type: 'integer' } })
+        expect(subject('double[]')).toEqual({ type: 'array', items: { type: 'number', format: 'double' } })
+        expect(subject('decimal[]')).toEqual({ type: 'array', items: { type: 'number', format: 'decimal' } })
+        expect(subject('date[]')).toEqual({ type: 'array', items: { type: 'string', format: 'date' } })
+        expect(subject('date-time[]')).toEqual({
+          type: 'array',
+          items: { type: 'string', format: 'date-time' },
+        })
       })
-      expect(subject({ type: 'decimal[]' })).toEqual({
-        type: 'array',
-        items: { type: 'number', format: 'decimal' },
-      })
-      expect(subject({ type: 'date[]' })).toEqual({
-        type: 'array',
-        items: { type: 'string', format: 'date' },
-      })
-      expect(subject({ type: 'date-time[]' })).toEqual({
-        type: 'array',
-        items: { type: 'string', format: 'date-time' },
+
+      it('can handle object primitive arrays', () => {
+        expect(subject({ type: 'string[]' })).toEqual({ type: 'array', items: { type: 'string' } })
+        expect(subject({ type: 'boolean[]' })).toEqual({ type: 'array', items: { type: 'boolean' } })
+        expect(subject({ type: 'number[]' })).toEqual({ type: 'array', items: { type: 'number' } })
+        expect(subject({ type: 'integer[]' })).toEqual({ type: 'array', items: { type: 'integer' } })
+        expect(subject({ type: 'double[]' })).toEqual({
+          type: 'array',
+          items: { type: 'number', format: 'double' },
+        })
+        expect(subject({ type: 'decimal[]' })).toEqual({
+          type: 'array',
+          items: { type: 'number', format: 'decimal' },
+        })
+        expect(subject({ type: 'date[]' })).toEqual({
+          type: 'array',
+          items: { type: 'string', format: 'date' },
+        })
+        expect(subject({ type: 'date-time[]' })).toEqual({
+          type: 'array',
+          items: { type: 'string', format: 'date-time' },
+        })
       })
     })
   })
