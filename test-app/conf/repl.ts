109c4b04982a2e8d2f90c06a89d760c4c6ca8955
avocadojesus <jgarbey@gmail.com>
2024-09-06 11:52:20 -0700

    update to latest dream repl
    related https://rvohealth.atlassian.net/browse/PDTC-5994

--- test-app/conf/repl.ts
+++ test-app/conf/repl.ts
@@ -2,8 +2,10 @@ import '../../src/helpers/loadEnv'
 
 import { loadRepl } from '@rvohealth/dream'
 import * as repl from 'node:repl'
+import initializePsychicApplication from '../cli/helpers/initializePsychicApplication'
 
 const replServer = repl.start('> ')
 export default (async function () {
-  await loadRepl(replServer.context)
+  await initializePsychicApplication()
+  loadRepl(replServer.context)
 })()
