c4c870acb461deb616415f0c74508897762241f3
Trace Helms <tracehelms@gmail.com>
2024-09-06 15:40:00 -0700

    fix remaining tests

--- spec/unit/router/routes.spec.ts
+++ spec/unit/router/routes.spec.ts
@@ -1,5 +1,7 @@
 import PsychicServer from '../../../src/server'
 import PsychicRouter from '../../../src/router'
+import ApiPetsController from '../../../test-app/app/controllers/Api/Users/PetsController'
+import UsersController from '../../../test-app/app/controllers/UsersController'
 
 describe('PsychicRouter', () => {
   describe('#routes', () => {
@@ -11,16 +13,19 @@ describe('PsychicRouter', () => {
     })
 
     it('correctly namespaces nested routes as members of the resource', () => {
-      router.resources('users', r => {
-        r.resources('pets')
+      router.namespace('api', r => {
+        r.resources('users', r => {
+          r.resources('pets')
+        })
       })
       router.commit()
       expect(router.routes).toEqual(
         expect.arrayContaining([
           {
             httpMethod: 'put',
-            path: '/users/:userId/pets/:id',
-            controllerActionString: 'Users/Pets#update',
+            path: '/api/users/:userId/pets/:id',
+            controller: ApiPetsController,
+            action: 'update',
           },
         ]),
       )
@@ -28,7 +33,7 @@ describe('PsychicRouter', () => {
 
     it('correctly applies nested crud routes within resources', () => {
       router.resources('users', r => {
-        r.get('pets', 'Users#pets')
+        r.get('pets', UsersController, 'ping')
       })
       router.commit()
       expect(router.routes).toEqual(
@@ -36,7 +41,8 @@ describe('PsychicRouter', () => {
           {
             httpMethod: 'get',
             path: '/users/:id/pets',
-            controllerActionString: 'Users#pets',
+            controller: UsersController,
+            action: 'ping',
           },
         ]),
       )
