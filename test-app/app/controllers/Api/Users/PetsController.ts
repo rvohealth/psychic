85c1a28f41550b35aa5ec5963983fd3fcd027925
avocadojesus <jgarbey@gmail.com>
2024-10-14 09:43:33 -0700

    fix openapi response conflation
    modifying the default response for a single endpoint should not have any
    affect on other endpoint responses

--- test-app/app/controllers/Api/Users/PetsController.ts
+++
@@ -1,7 +0,0 @@
-import ApplicationController from '../../../../src/app/controllers/ApplicationController'
-
-export default class ApiPetsController extends ApplicationController {
-  public count() {
-    this.noContent()
-  }
-}
