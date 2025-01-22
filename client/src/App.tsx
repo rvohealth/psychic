599008f149e08484103fc93c940265c592089005
avocadojesus <jgarbey@gmail.com>
2025-01-22 15:53:25 -0800

    fix disconnect issues with cable
    related https://rvohealth.atlassian.net/browse/PDTC-6719
    related https://rvohealth.atlassian.net/browse/PDTC-6821

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,22 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import './App.css'
+import { io } from 'socket.io-client'
+import baseWsUrl from './helpers/baseWsUrl'
 import viteEnvValue from './helpers/viteEnvValue'
 
-function App() {
+import './App.css'
+import { Route, Routes, useParams } from 'react-router'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/socket-test/:token" element={<SocketTestPage />} />
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  )
+}
+
+function HomePage() {
   const [message, setMessage] = useState('')
 
   const port = viteEnvValue('VITE_PSYCHIC_ENV') === 'test' ? 7778 : 7777
@@ -17,7 +30,35 @@ function App() {
     void getMessage()
   }, [])
 
-  return <div>{message}</div>
+  return (
+    <div>
+      <p>{message}</p>
+    </div>
+  )
 }
 
-export default App
+function SocketTestPage() {
+  const [websocketMessage, setWebsocketMessage] = useState('')
+  const params = useParams()
+  const authToken = params.token as string
+
+  useEffect(() => {
+    const socket = io(baseWsUrl(), {
+      auth: {
+        token: authToken,
+      },
+      withCredentials: true,
+      transports: ['websocket'],
+    })
+
+    socket.on('/ops/connection-success', () => {
+      setWebsocketMessage('websockets connected')
+    })
+  }, [])
+
+  return (
+    <div>
+      <p>{websocketMessage}</p>
+    </div>
+  )
+}
