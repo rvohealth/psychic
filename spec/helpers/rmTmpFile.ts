46719b2ae60d60a39019994c21f4bbc692ad71ca
avocadojesus <jgarbey@gmail.com>
2025-01-31 12:52:21 -0800

    bump version to 0.20.0

--- spec/helpers/rmTmpFile.ts
+++ spec/helpers/rmTmpFile.ts
@@ -2,7 +2,7 @@ import fs from 'fs/promises'
 import path from 'path'
 import PsychicApplication from '../../src/psychic-application'
 
-export default async function readTmpFile() {
+export default async function rmTmpFile() {
   try {
     const psychicApp = PsychicApplication.getOrFail()
     return await fs.rm(path.join(psychicApp.apiRoot, 'spec/tmp.txt'))
