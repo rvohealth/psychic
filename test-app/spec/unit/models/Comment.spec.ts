4aabb4dca743e9b095a0d43cd246c716ca2650b1
avocadojesus <jgarbey@gmail.com>
2025-03-07 23:18:55 -0800

    fixup

--- test-app/spec/unit/models/Comment.spec.ts
+++
@@ -1,3 +0,0 @@
-describe('Comment', () => {
-  it.todo('add a test here to get started building Comment')
-})
--- test-app/spec/unit/models/Post.spec.ts
+++
@@ -1,3 +0,0 @@
-describe('Post', () => {
-  it.todo('add a test here to get started building Post')
-})
