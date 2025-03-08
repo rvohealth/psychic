09a22adfb828e61bb41dcab91a43026dc593cdb1
avocadojesus <jgarbey@gmail.com>
2025-03-07 22:11:15 -0800

    cleanup

--- eslint.config.js
+++ eslint.config.js
@@ -25,19 +25,6 @@ const config = typescriptEslint.config(
     ],
   },
 
-  // {
-  //   rules: {
-  //     'no-unexpected-multiline': 'off',
-  //     // '@typescript-eslint/no-explicit-any': 'off',
-  //     // '@typescript-eslint/no-unsafe-assignment': 'off',
-  //     // '@typescript-eslint/no-unsafe-member-access': 'off',
-  //     // '@typescript-eslint/no-unsafe-return': 'off',
-  //     // '@typescript-eslint/no-unsafe-argument': 'off',
-  //     '@typescript-eslint/no-unsafe-call': 'off',
-  //     // '@typescript-eslint/no-this-alias': 'off',
-  //   },
-  // },
-  //
   {
     files: ['**/*.ts'],
     languageOptions: {
