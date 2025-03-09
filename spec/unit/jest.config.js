6b625e991b5ef39889adeb3d1e913ce9d5fb2da8
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-03-09 08:08:07 -0500

    failed attempts

--- spec/unit/jest.config.js
+++ spec/unit/jest.config.js
@@ -1,13 +1,24 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
   restoreMocks: true,
   clearMocks: true,
   resetMocks: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: { '^.+\\.tsx?$': 'ts-jest' },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        babelConfig: true,
+        tsconfig: {
+          module: 'commonjs',
+        },
+      },
+    ],
+  },
   setupFiles: [],
   setupFilesAfterEnv: ['<rootDir>setup/hooks.ts'],
   globalSetup: '<rootDir>setup/beforeAll.ts',
   globalTeardown: '<rootDir>setup/afterAll.ts',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 }
