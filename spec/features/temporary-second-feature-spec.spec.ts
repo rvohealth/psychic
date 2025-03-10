2289717ced09330ee9421e250e3d6b928ecb016b
avocadojesus <jgarbey@gmail.com>
2025-03-10 11:06:03 -0700

    fixup

--- spec/features/temporary-second-feature-spec.spec.ts
+++ spec/features/temporary-second-feature-spec.spec.ts
@@ -1,9 +1,7 @@
-import launchBrowser from './setup/helpers/launchBrowser'
+import { visit } from '@rvohealth/psychic-spec-helpers'
 
 describe('ensures that a second feature spec can run', () => {
   it('can run multiple feature specs', async () => {
-    const browser = await launchBrowser()
-    const page = await browser.newPage()
-    await page.goto('http://localhost:3000')
+    await visit('/')
   })
 })
