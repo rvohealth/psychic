f02c9e5ace6262c231de8fb24eab2e06a2e09244
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-04-04 18:37:25 -0500

    Make error match conf/app

--- src/error/psychic-application/init-missing-package-manager.ts
+++ src/error/psychic-application/init-missing-package-manager.ts
@@ -10,8 +10,8 @@ must set packageManager when initializing a new PsychicApplication.
 within conf/app.ts, you must have a call to "#set('packageManager', '<YOUR_CHOSEN_PACKAGE_MANAGER>')", i.e.
 
   // conf/app.ts
-  export default async (app: PsychicApplication) => {
-    await app.set('packageManager', 'yarn')
+  export default async (psy: PsychicApplication) => {
+    psy.set('packageManager', 'yarn')
   }
     `
   }
