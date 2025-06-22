eb1c029a5a8031672e8e56282040db42f448a262
avocadojesus <jgarbey@gmail.com>
2025-06-22 12:19:10 -0700

    add proxy method to easily tap into `use` within express
    this restricts the use functionality to strictly an overload where the
    only argument passed to use is a request handler. Other use cases can
    still be implemented, but will need to go through the
    `psy.on('server:init:*')` hooks to execute it.

--- spec/unit/server/hooks.spec.ts
+++ spec/unit/server/hooks.spec.ts
@@ -1,5 +1,5 @@
 import { PsychicServer } from '../../../src/index.js'
-import { PsychicHookEventType } from '../../../src/psychic-app/types.js'
+import { specRequest as request } from '@rvoh/psychic-spec-helpers'
 
 describe('PsychicServer hooks', () => {
   let server: PsychicServer
@@ -9,7 +9,7 @@ describe('PsychicServer hooks', () => {
     server = new PsychicServer()
   })
 
-  function expectHookCalled(hookEventType: PsychicHookEventType) {
+  function expectHookCalled(hookEventType: string) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
     expect((process.env as any).__PSYCHIC_HOOKS_TEST_CACHE.split(',')).toEqual(
       expect.arrayContaining([hookEventType]),
@@ -21,6 +21,14 @@ describe('PsychicServer hooks', () => {
     expectHookCalled('server:init:before-middleware')
     expectHookCalled('server:init:after-middleware')
     expectHookCalled('server:init:after-routes')
+  })
+
+  it('processes hooks for "use"', async () => {
+    await request.init(PsychicServer)
+    await request.get('/ok', 200)
+    expectHookCalled('use')
+    expectHookCalled('use:before-middleware')
+    expectHookCalled('use:after-middleware')
   })
 
   it('processes hooks for server:start', async () => {
--- src/psychic-app/types.ts
+++ src/psychic-app/types.ts
@@ -13,5 +13,8 @@ export type PsychicHookEventType =
 
 export type PsychicAppInitializerCb = (psychicApp: PsychicApp) => void | Promise<void>
 
+export const PsychicUseEventTypeValues = ['before-middleware', 'after-middleware', 'after-routes'] as const
+export type PsychicUseEventType = (typeof PsychicUseEventTypeValues)[number]
+
 type Only<T, U> = T & Partial<Record<Exclude<keyof U, keyof T>, never>>
 export type Either<T, U> = Only<T, U> | Only<U, T>
