426dc4bd78511b8235fa621cf3d02febe254569c
avocadojesus <jgarbey@gmail.com>
2025-07-31 12:00:51 -0700

    fix enum generation regression after mutli db engine support

--- src/bin/helpers/enumsAndTheirValues.ts
+++ src/bin/helpers/enumsAndTheirValues.ts
@@ -1,11 +1,11 @@
 import { untypedDb } from '@rvoh/dream'
 import { sql } from 'kysely'
 
-export default async function enumsAndTheirValues() {
+export default async function enumsAndTheirValues(connectionName: string = 'default') {
   const { rows } = await sql<{ enumType: string; enumLabel: string }>`
 SELECT pg_type.typname AS enum_type, pg_enum.enumlabel AS enum_label FROM pg_type JOIN pg_enum ON pg_enum.enumtypid = pg_type.oid;
 
-`.execute(untypedDb('primary'))
+`.execute(untypedDb(connectionName, 'primary'))
 
   const rowData: Record<string, string[]> = {}
 
