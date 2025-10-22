24ea0641440ae411b9e2b4520f45fe27e8fe74a1
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-22 07:59:56 -0500

    internal exports

--- src/package-exports/index.ts
+++ src/package-exports/index.ts
@@ -1,38 +1,30 @@
+// utils
 import _pluralize from 'pluralize-esm'
 export const pluralize = _pluralize
-
-export { default as PsychicBin } from '../bin/index.js'
-export { default as PsychicCLI } from '../cli/index.js'
-export { BeforeAction, OpenAPI } from '../controller/decorators.js'
-export {
-  default as PsychicController,
-  type PsychicOpenapiControllerConfig,
-  type PsychicOpenapiNames,
-} from '../controller/index.js'
-export { default as PsychicDevtools } from '../devtools/PsychicDevtools.js'
-export { default as envLoader } from '../env/Loader.js'
-export { default as I18nProvider } from '../i18n/provider.js'
-
-export { default as generateController } from '../generate/controller.js'
-export { default as generateResource } from '../generate/resource.js'
-export { default as cookieMaxAgeFromCookieOpts } from '../helpers/cookieMaxAgeFromCookieOpts.js'
-export { default as pathifyNestedObject } from '../helpers/pathifyNestedObject.js'
-export { default as sanitizeString } from '../helpers/sanitizeString.js'
-
 export { default as colorize } from '../cli/helpers/colorize.js'
-export { default as PsychicLogos } from '../cli/helpers/PsychicLogos.js'
+export { default as cookieMaxAgeFromCookieOpts } from '../helpers/cookieMaxAgeFromCookieOpts.js'
 
-export { default as PsychicImporter } from '../psychic-app/helpers/PsychicImporter.js'
+// types
+export { type PsychicOpenapiControllerConfig, type PsychicOpenapiNames } from '../controller/index.js'
 export {
-  default as PsychicApp,
   type DefaultPsychicOpenapiOptions,
   type NamedPsychicOpenapiOptions,
   type PsychicAppInitOptions,
 } from '../psychic-app/index.js'
 export { type UUID } from '../psychic-app/types.js'
-export { default as PsychicRouter } from '../router/index.js'
 export { type HttpMethod } from '../router/types.js'
-export { createPsychicHttpInstance as getPsychicHttpInstance } from '../server/helpers/startPsychicServer.js'
+
+// default
+export { BeforeAction, OpenAPI } from '../controller/decorators.js'
+export { default as PsychicController } from '../controller/index.js'
+export { default as I18nProvider } from '../i18n/provider.js'
+
+export { default as pathifyNestedObject } from '../helpers/pathifyNestedObject.js'
+export { default as sanitizeString } from '../helpers/sanitizeString.js'
+
+export { default as PsychicApp } from '../psychic-app/index.js'
+
+export { default as PsychicRouter } from '../router/index.js'
 export { default as PsychicServer } from '../server/index.js'
 export { default as Params } from '../server/params.js'
 export { default as PsychicSession } from '../session/index.js'
