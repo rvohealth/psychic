5d5908bd4d390849e445270103b1f769091c351f
Daniel Nelson <844258+daniel-nelson@users.noreply.github.com>
2025-10-22 08:02:26 -0500

    util exports

--- src/package-exports/index.ts
+++ src/package-exports/index.ts
@@ -1,19 +1,9 @@
-// utils
-import _pluralize from 'pluralize-esm'
-export const pluralize = _pluralize
-export { default as colorize } from '../cli/helpers/colorize.js'
-export { default as cookieMaxAgeFromCookieOpts } from '../helpers/cookieMaxAgeFromCookieOpts.js'
-
-// default
 export { BeforeAction, OpenAPI } from '../controller/decorators.js'
 export { default as PsychicController } from '../controller/index.js'
-export { default as I18nProvider } from '../i18n/provider.js'
-
 export { default as pathifyNestedObject } from '../helpers/pathifyNestedObject.js'
 export { default as sanitizeString } from '../helpers/sanitizeString.js'
-
+export { default as I18nProvider } from '../i18n/provider.js'
 export { default as PsychicApp } from '../psychic-app/index.js'
-
 export { default as PsychicRouter } from '../router/index.js'
 export { default as PsychicServer } from '../server/index.js'
 export { default as Params } from '../server/params.js'
